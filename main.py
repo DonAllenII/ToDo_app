59dad415ec296009d472676b1edd8faa07694ad8
DonAllenII <don.a.alexanderii@gmail.com>
2025-05-26 12:41:56 -0400

    instructions

---
+++ main.py
@@ -0,0 +1,22 @@
+#To Do Application
+
+#Build a simple command line interface (CLI) welcoming users
+#and displays a menu with options to add, view, and delete tasks,
+#or quit the application
+#The tasks should be stored in a python list
+
+#Core Features
+#Add tasks
+#View Tasks
+#Delete Tasks
+#Quit the applciation
+
+#User interaction
+# use input() to capture user selections and ensure proper input validation 
+#to handle invalid choices
+
+#Error Hanlding
+
+#code Organization
+#testing and Debugging 
+
